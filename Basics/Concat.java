283cd36dc18f1fad39216b72432b7ad9809b93f4
Kuheli Bera <133209155+kuheli31@users.noreply.github.com>
2025-07-10 11:43:25 +0530

    Update Concat.java

--- Basics/Concat.java
+++ Basics/Concat.java
@@ -2,6 +2,6 @@
 public class Concat{
 	public static void main(String[] param)
 	{
-	System.out.+println("Concatenation of two arguments is: " + param[0]+param[1]);
+	System.out.println("Concatenation of two arguments is: " + param[0]+param[1]);
 	}
 }
